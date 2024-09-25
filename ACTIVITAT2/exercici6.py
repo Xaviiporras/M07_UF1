e9801c81a832e21a878997b326e3c08d3ab37852
Xavi Porras <2023_xavier.porras@iticbcn.cat>
2024-09-25 20:40:59 +0200

    Exercici6 afegit

---
+++ ACTIVITAT2/exercici6.py
@@ -0,0 +1,13 @@
+numWhile = 1
+
+print("BUCLE WHILE:")
+while (numWhile < 101):
+    print(numWhile)
+    numWhile = numWhile + 1
+
+print("")
+
+print("BUCLE FOR:")
+
+for i in range(1, 101):
+    print(i)
