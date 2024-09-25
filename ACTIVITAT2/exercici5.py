49efd03390c9367ff8a5e2e88d4703f3bdf78e9d
Xavi Porras <2023_xavier.porras@iticbcn.cat>
2024-09-25 20:36:10 +0200

    Exercici5 afegit

---
+++ ACTIVITAT2/exercici5.py
@@ -0,0 +1,8 @@
+valor1 = float(input("Introdueix un numero: "))
+
+result = valor1 % 2
+
+if (result == 0):
+    print("El numero introduit es parell")
+else:
+    print("El numero introduir es senar")
