be29fab7fe9a8aab745998340dac46045d665330
Xavi Porras <2023_xavier.porras@iticbcn.cat>
2024-09-25 20:18:49 +0200

    Exercici 3 afegit

---
+++ ACTIVITAT2/exercici3.py
@@ -0,0 +1,7 @@
+valor1 = float(input("Introdueix el valor 1: "))
+valor2 = float(input("Introdueix el valor 2: "))
+
+if (valor1 > valor2):
+    print(f"El valor més gran es {valor1}")
+else:
+    print(f"El valor més gran es {valor2}")
